0ca62e9b1113d17d9fc46f2fed7313b7be9690f5
Abhishek Baliyan <abhi.info.dev@gmail.com>
2017-04-05 18:33:05 +0530

    Mega commit 1. Cloud Storage 2. Getting a Image from a User 3. Storing an Image

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -6,17 +6,43 @@ import './ProfileCard.css';
 class ProfileCard extends Component {
   constructor(props) {
     super(props);
+
+    this.storageRef = storage.ref('/user-images').child(props.uid);
+    this.userRef = database.ref('/users').child(props.uid);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    const file = event.target.files[0];
+    const uploadTask = this.storageRef.child(file.name)
+                                      .put(file, { contentType: file.type });
+
+    uploadTask.then((snapshot) => {
+      this.userRef.child('photoURL').set(snapshot.downloadURL);
+    });
   }
 
   render() {
+    const { photoURL, displayName } = this.props.user;
     return (
       <article className="ProfileCard">
+        <img
+          className="Profile--photo"
+          src={ photoURL }
+        />
+        <h3>{ displayName }</h3>
+        <FileInput
+          accept=".png,.gif,.jpg"
+          placeholder="Select an image"
+          onChange={this.handleSubmit}
+        />
       </article>
     );
   }
 }
 
 ProfileCard.propTypes = {
+  user: PropTypes.object,
   displayName: PropTypes.string,
   email: PropTypes.string,
   imageName: PropTypes.string,
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  storageBucket: '',
-  messagingSenderId: ''
+  apiKey: "AIzaSyDyYiJ9KsWJEMcazHM_-8DWLqreWR2fzO4",
+  authDomain: "social-animals-3e673.firebaseapp.com",
+  databaseURL: "https://social-animals-3e673.firebaseio.com",
+  projectId: "social-animals-3e673",
+  storageBucket: "social-animals-3e673.appspot.com",
+  messagingSenderId: "537461544415"
 };
 
 firebase.initializeApp(config);
